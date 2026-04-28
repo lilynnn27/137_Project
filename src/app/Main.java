498cb9a60945c0ffc46491e62fafe6fa63199da3
lilynnn27 <eaamistoso@up.edu.ph>
2026-04-28 14:09:05 +0800

    ui: game over modal font

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,6 +5,8 @@ import app.screens.LandingPageScreen;
 import app.screens.MultiplayerScreen;
 import app.screens.SinglePlayerScreen;
 import javafx.application.Application;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.effect.GaussianBlur;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -58,6 +60,19 @@ public class Main extends Application {
         GamePlayScreen gamePlayScreen = new GamePlayScreen(this);
         mainScene.setRoot(gamePlayScreen.getRoot());
         gamePlayScreen.getRoot().requestFocus();
+    }
+
+    public void setBackgroundBlur(boolean apply){
+        if(apply){
+            GaussianBlur blur = new GaussianBlur(15); // Adjust radius for more/less blur
+            ColorAdjust darken = new ColorAdjust();
+            darken.setBrightness(-0.5); // Darken by 50%
+            
+            blur.setInput(darken);
+            window.getScene().getRoot().setEffect(blur);
+        } else {
+            window.getScene().getRoot().setEffect(null);
+        }
     }
     
     public void exitGame() {
